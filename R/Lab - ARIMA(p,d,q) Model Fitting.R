ba0312eb35860e66b79c870caa7e0dec4e911bf0
Kristine Dinh <kdinh@arrowheadgrp.com>
2020-10-28 19:08:22 -0700

    updated lab 2

--- R/Lab - ARIMA(p,d,q) Model Fitting.R
+++ R/Lab - ARIMA(p,d,q) Model Fitting.R
@@ -1,3 +1,52 @@
+#### Example 1: Monthly Sunspot Data, AR(p) ####
+
+par(mfrow=c(2,2))
+
+plot(sunspot.month)
+
+acf(sunspot.month)
+
+acf(sunspot.month, type="partial")
+
+# AR modeling with order selection.
+sunfit1 <- ar(sunspot.month)
+
+sunfit1
+
+# As far as I can tell, there are no model diagnostics for ar. What should you look at?
+# Fit a model with the arima Call the fit sunfit2 For model diagnostics for arima function try:
+sunfit2 <- arima(sunspot.month, order = c(6, 0, 0))
+sunfit2
+## 6 coefficent 
+
+## diagnostic plots 
+tsdiag(sunfit2)
+# 1) standardized residual: we should we no pattern here 
+# there is chunk of residuals sometimes 
+# there is definately some pattern 
+# sometimes there is little residuals and 
+# sometimes there is larger residuals 
+
+# 2) ACF plots: there are still spikes our after lag 1 
+
+# 3) p values: high p values mostly, 
+# however there are low p values at lag 9 and 10
+
+## now try to fit an ar 29 
+sunfit3 <- arima(sunspot.month, order = c(29, 0, 0))
+sunfit3
+
+tsdiag(sunfit3)
+# 1) standard residual: still see pattern in error. error should be iid
+# 2) ACF: acf cut off after 1 
+# 3) pvalues: correlation are all higher 
+
+## QQ plot of residuals 
+par(mfrow = c(1,1))
+qqnorm(sunfit3$residuals)
+qqline(sunfit3$residuals, col = "red")
+# residual is not normal 
+
 #### Example 2: Model Identification and Fitting ####
 # Example for model identification and forecasting 
 # Generate some training data.
@@ -55,6 +104,90 @@ par(mfrow=c(1,1))
 qqnorm(fit$residuals, pch = 1, frame = FALSE)
 qqline(fit$residuals, col = "red", lwd = 2)
 
-library("car")
-qqPlot(fit$residuals)
+## OR 
+car::qqPlot(fit$residuals)
+
+## fit models ar - fits with y-w (mon), AIC for model selection 
+## arima - fit with MLE; return AIC and summary 
+## tsdiag will give diagnostic plots 
+## qqnorm will give us qq plot to check for normality 
+
+
+#### Example color dataset ####
+
+library(TSA)
+data(color)
+
+par(mfrow=c(2,2))
+plot(color)
+# reasonably stationary  
+# there is going up and down trend but we have less points 
+
+plot(diff(color))
+acf(color)
+# damp since wave pattern 
+
+acf(color, type = "partial")
+# pacf cutoff after 1 
+
+ar(color)
+# one coeficient is good 
+
+color.fit1 <- arima(color, order = c(1,0,0)) 
+
+tsdiag(color.fit1)
+# residual looks randome 
+# ACF drop off after 0 
+# high correlation with high p values 
+
+qqnorm(color.fit1$residuals, pch = 1, frame = FALSE)
+qqline(color.fit1$residuals, col = "red", lwd = 2)
+# normality doens't look too bad 
+# a little bit off but not that bad 
+
+
+#### Example oil ####
+
+#Read in data
+oil.price <- read.table("https://edoras.sdsu.edu/~babailey/stat673/oil.price.dat", header=TRUE)
+
+oil <- ts(oil.price)
+
+par(mfrow=c(2,2))
+plot(oil, main="Oil")
+
+plot(log(oil), main="Log Oil")
+
+plot(diff(oil), main="Diff Oil")
+
+plot(diff(log(oil)), main="Diff Log Oil")
+
+# diff log oil looks the most stationary 
+
+par(mfrow=c(3,2))
+acf(oil,main='Sample ACF of the Oil Price TS')
+acf(log(oil), main="ACF of log Oil")
+
+acf(diff(oil), main="ACF of Diff Oil")
+acf(diff(log(oil)), main="ACF of Diff Log Oil") 
+
+pacf(diff(oil), main="PACF of Diff Oil")
+pacf(diff(log(oil)), main="PACF of Diff Log Oil") 
+
+#Models: ARIMA(2,1,0) or ARIMA(0,1,1)
+# d = 1 when taking diff one time
+
+oil.fit1 <- arima(log(oil), order = c(2,1,0)) 
+oil.fit1
+tsdiag(oil.fit1)
+qqnorm(oil.fit1$residuals, pch = 1, frame = FALSE)
+qqline(oil.fit1$residuals, col = "red", lwd = 2)
+
+
+oil.fit2 <- arima(log(oil), order = c(0,1,1)) 
+oil.fit2
+tsdiag(oil.fit2)
+
+# pick the smaller AIC if the models are too similar 
+# -518 is smaller than -517 
 
